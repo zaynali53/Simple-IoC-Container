266e80f6bf0ab16ef10458a624c7b3b9b457f6b1
Zayn Ali <plz442011@gmail.com>
2014-12-08 09:31:21 +0500

    Update IoC.php

--- IoC.php
+++ IoC.php
@@ -1,14 +1,16 @@
 <?php 
 
+namespace Mark;
+
 class IoC {
 
     protected static $registry = array();
  
     /**
      * Add a new resolver to the registry array.
-     * @param   string  $name       (The key)
-     * @param   object  $closure    (Closure that creates instance)
-     * @return  void
+     * @param  string $name 	(The key)
+     * @param  object $closure 	(Closure that creates instance)
+     * @return void
      */
     public static function register($name, callable $closure)
     {
@@ -17,24 +19,25 @@ class IoC {
  
     /**
      * Create the instance
-     * @param   string  $name   (The key)
-     * @return  mixed
+     * @param  string $name (The key)
+     * @return mixed
      */
-    public static function resolve($name)
+    public static function make($name, $args = NULL)
     {
-        if ( static::registered($name) )
+        if (static::registered($name))
         {
             $name = static::$registry[$name];
-            return $name();
+
+            return $name($args);
         }
  
-        throw new Exception("$name isn't registered");
+        throw new \Exception("$name isn't registered");
     }
  
     /**
      * Determine whether the key is registered
-     * @param   string  $name   (The key)
-     * @return  bool            (Whether the key exists or not)
+     * @param  string $name (The key)
+     * @return bool 		(Whether the key exists or not)
      */
     public static function registered($name)
     {
