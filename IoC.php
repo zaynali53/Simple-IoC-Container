86e62ec3d8a54b2c048eff8dc2e328a0dfeaa6b6
Zayn Ali <plz442011@gmail.com>
2014-12-08 12:09:43 +0500

    Update IoC.php

--- IoC.php
+++ IoC.php
@@ -1,24 +1,29 @@
 <?php 
 /**
  * IoC Container (https://github.com/zaynali53/Simple-IoC-Container)
- * @author: Zayn Ali (https://github.com/zaynali53)
+ * @author  Zayn Ali (https://github.com/zaynali53)
+ * @link    Facebook (https://facebook.com/zaynali53)
  */
- 
-class IoC {
 
-    protected static $registry = array();
- 
+abstract class IoC {
+
+    /**
+     * Container for registered callbacks
+     * @var mixed
+     */
+    protected static $registry = [];
+
     /**
      * Add a new resolver to the registry array.
-     * @param  string $name 	(The key)
-     * @param  object $closure 	(Closure that creates instance)
+     * @param  string $name 	 (The key)
+     * @param  object $callback  (Closure that creates instance)
      * @return void
      */
-    public static function register($name, callable $closure)
+    public static function register($name, callable $callback)
     {
-        static::$registry[$name] = $closure;
+        self::$registry[$name] = $callback;
     }
- 
+
     /**
      * Create the instance
      * @param  string $name (The key)
@@ -26,24 +31,24 @@ class IoC {
      */
     public static function make($name, $args = NULL)
     {
-        if (static::registered($name))
-        {
-            $name = static::$registry[$name];
+        if (self::isRegistered($name)) {
+
+            $name = self::$registry[$name];
 
             return $name($args);
         }
- 
-        throw new \Exception("$name isn't registered");
+
+        throw new \Exception("Error: Key \"$name\" is not registered!\n");
     }
- 
+
     /**
      * Determine whether the key is registered
      * @param  string $name (The key)
      * @return bool 		(Whether the key exists or not)
      */
-    public static function registered($name)
+    public static function isRegistered($name)
     {
-        return array_key_exists($name, static::$registry);
+        return array_key_exists($name, self::$registry);
     }
 
 }
